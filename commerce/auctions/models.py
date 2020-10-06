3e2c9adb99c5381fc1a0a502381db3dbe279e22a
root <vivekkr0311@gmail.com>
2020-10-06 15:46:29 +0530

    Frontend Work

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -19,6 +19,15 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.id} {self.item_name}"
+
+class All_Won(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner_name = models.CharField(max_length=64)
+    item_name = models.CharField(max_length=64)
+    category = models.CharField(max_length=64)
+    description = models.TextField(max_length=64, blank=True)
+    link = models.CharField(max_length=64, default=None, blank=True, null=True)
+    time = models.CharField(max_length=64)
     
 
 class Watch_list(models.Model):
@@ -26,3 +35,21 @@ class Watch_list(models.Model):
     p_ID = models.IntegerField()
 
 
+class Comment_Table(models.Model):
+    person = models.ForeignKey(User, on_delete=models.CASCADE, related_name="person_in_comments")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_in_comments")
+    comment = models.TextField()
+    time = models.DateTimeField(default=timezone.now)
+
+class Bid(models.Model):
+    Bid = models.IntegerField()
+    bid_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="all_bids")
+    on_product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="Bid")
+
+class Winner(models.Model):
+    won_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="winings")
+    product_won = models.ForeignKey(All_Won, on_delete=models.CASCADE, related_name="item_won")
+    on_price = models.IntegerField()
+
+
+    
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -23,6 +23,9 @@ class Change_Bid_Form(forms.Form):
 
 class Comment(forms.Form):
     comment = forms.CharField(widget=forms.Textarea)
+
+class Bid_Form(forms.Form):
+    Bid = forms.IntegerField()
     
     
 
@@ -94,21 +97,160 @@ def register(request):
 
 def item_details(request, item_id):
     item = Product.objects.get(pk=item_id)
-
+    all_comments = Comment_Table.objects.filter(product=item_id)
+    all_bids = Bid.objects.filter(on_product=item)
     if request.method == "POST":
         comment = Comment(request.POST)
         if comment.is_valid():
             comment = comment.cleaned_data["comment"]
 
-            return HttpResponse("Comment submitted.")
+            user = request.user.username
+            u_ID = User.objects.get(username=user)
+            p_ID = Product.objects.get(pk=item_id)
+            c = Comment_Table(person=u_ID, product=p_ID, comment=comment)
+            c.save()
+
+            return render(request, "auctions/item_details.html", {
+                "item":item,
+                "comment":Comment(),
+                "list_of_comments":all_comments,
+                "all_bids":all_bids
+            })
         #else:
         #   return HttpResponse("Comment is not valid.")
 
     return render(request, "auctions/item_details.html", {
         "item":item,
-        "comment":Comment()
+        "comment":Comment(),
+        "list_of_comments":all_comments,
+        "all_bids":all_bids
     })
+
+def show_categories(request):
+    products = Product.objects.all()
+    categories = []
+    for product in products:
+        if product.category not in categories:
+            categories.append(product.category)
+        else:
+            continue
+
     
+    return render(request, "auctions/category.html", {
+        "categories":categories
+    })
+
+def category_search(request):
+    category = request.POST["category"]
+    
+    items = Product.objects.filter(category=category)
+
+    return render(request, "auctions/category_search.html", {
+        "items":items
+    })
+
+@login_required
+def Bid_Now(request, product_id):
+    product_object = Product.objects.get(pk=product_id)
+    user_object = User.objects.get(username=request.user.username)
+    if request.method == "POST":
+        bid_raw_data = Bid_Form(request.POST)
+        if bid_raw_data.is_valid():
+            bid = bid_raw_data.cleaned_data["Bid"]
+
+            p = Product.objects.get(pk=product_id)
+            if bid >= p.price:
+                Bid_object = Bid(Bid=bid, bid_by=user_object, on_product=product_object)
+                Bid_object.save()
+
+                return redirect("index")
+            else:
+                return render(request, "auctions/Biding.html", {
+                    "form":Bid_Form(),
+                    "product":product_object,
+                    "message":"To win this item, bid with more amount."
+                })
+    else:
+        return render(request, "auctions/Biding.html", {
+            "form":Bid_Form(),
+            "product":product_object
+        })
+
+
+
+
+    return render(request, "auctions/Biding.html", {
+        "form":Bid_Form()
+    })
+
+@login_required
+def delete_bid(request, item_id):
+
+    p = Product.objects.get(pk=item_id)
+    w = Watch_list.objects.get(p_ID=item_id)
+    p.delete()
+    w.delete()
+    return redirect("my_products", request.user.username)
+
+@login_required
+def close_bid(request, item_id):
+    p = Product.objects.get(pk=item_id)
+    watchlist_object = Watch_list.objects.filter(p_ID=item_id)
+    for each_object in watchlist_object:
+        each_object.delete()
+    '''try:
+        bid_object = Bid.objects.filter(on_product=p)
+        return render(request, "auctions/bid_closing_page.html", {
+            "all_bids_on_item":bid_object
+        })
+    except Bid.DoesNotExist:
+        return HttpResponse("No winner for your item.")'''
+    bid_object = Bid.objects.filter(on_product=p)
+    max_bid = max(single_object.Bid for single_object in bid_object)
+
+
+    #Getting winner's user object and product object.
+    bid_object_2 = Bid.objects.get(Bid=max_bid)
+    user_object = User.objects.get(username=bid_object_2.bid_by)
+    product_won = Product.objects.get(pk=item_id)
+    
+
+    #Copying all data to won.
+    won_product = All_Won()
+    won_product.owner_name = product_won.owner_name
+    won_product.item_name = product_won.item_name
+    won_product.category = product_won.category
+    won_product.description = product_won.description
+    won_product.link = product_won.link
+    won_product.time = product_won.time
+
+    won_product.save()
+
+    winner = Winner(won_by=user_object, product_won=won_product, on_price=max_bid)
+    winner.save()
+
+    #Deleting from all listing.
+    product_won.delete()
+    
+
+    return render(request, "auctions/bid_closing_page.html", {
+        "user":user_object,
+        "winner_bid":max_bid,
+        "won_product":won_product,
+        "all_bids_on_item":bid_object
+    })
+
+@login_required
+def closed_products(request):
+    user = request.user.username
+    
+    products_were = All_Won.objects.filter(owner_name=user)
+
+
+    return render(request, "auctions/winner_page.html", {
+        "items":products_were
+    })
+
 @login_required
 def my_products(request, username):
     id = User.objects.get(username=username).pk
@@ -177,7 +319,7 @@ def create_listing(request, username):
             p.save()
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "encyclopedia/create_listing.html", {
+            return render(request, "auctions/create_listing.html", {
                 "username":username,
                 "form": Add_product_Form()
             })
@@ -219,4 +361,19 @@ def change_Bid(request, product_ID):
 
 @login_required
 def your_winnings(request):
-    return render(request, "auctions/your_winnings.html")
+    user = User.objects.get(username=request.user.username)
+    
+    winner_objects = Winner.objects.filter(won_by=user)
+    
+    items_won = []
+    for each_object in winner_objects:
+        items_won.append(each_object.product_won)
+    
+    #won_product = winner_object.product_won.item_name
+    #on_price = winner_object.on_price
+    return render(request, "auctions/your_winnings.html",{
+        "items":items_won
+        #"on_product":on_product,
+        #"item":item
+    })
+    
